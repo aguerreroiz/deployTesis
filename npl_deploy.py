db8a5f7d2b72f3ec0c2eb8e7dd7303c974b1e829
aguerreroiz <aguerreroiz@gmail.com>
2022-08-13 18:39:10 -0500

    elementos barra lateral sin submenu home

--- npl_deploy.py
+++ npl_deploy.py
@@ -4,14 +4,22 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 
 # Instanciamos la aplicación
-
-nlp_deploy = dash.Dash(__name__)
+BS = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+nlp_deploy = dash.Dash(__name__,external_stylesheets=[BS])
 
 # Diseño de la aplicación
 
 nlp_deploy.layout = html.Div([
-    html.H1("test de Prueba")
-])
+    dbc.Nav([html.Div([html.H3('Titulo de la Barra')],className='sidebar-header'),
+             html.Ul([
+                 html.P(['Dummy Heading']),html.Li([html.A(['Home'],href="#homeSubmenu", className="dropdown-toggle"),html.Ul([],className="collapse list-unstyled", id="homeSubmenu")],className='active'),html.Li(['About']),html.Li(['Páginas']),
+                 html.Li(['Portafolio']),html.Li(['Contacto'])
+             ],className='list-unstyled components'),   
+    
+    ],id='sidebar'),
+    
+    html.Div([],id='contenido')
+],className='wrapper')
 
 
 # Ejecutar la aplicación
