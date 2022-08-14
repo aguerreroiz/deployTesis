005ad6debe5f487e7ac9b4276f431c4b3bdc60f7
aguerreroiz <aguerreroiz@gmail.com>
2022-08-13 21:32:06 -0500

    crear archivo de estilos barra lateral

--- npl_deploy.py
+++ npl_deploy.py
@@ -1,22 +1,27 @@
 # Importar las librerias requeridas
+from pydoc import classname
 import dash
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
 # Instanciamos la aplicación
-BS = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
-nlp_deploy = dash.Dash(__name__,external_stylesheets=[BS])
+#BS = "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+nlp_deploy = dash.Dash(__name__,external_stylesheets=[dbc.themes.SANDSTONE])
 
 # Diseño de la aplicación
 
 nlp_deploy.layout = html.Div([
     dbc.Nav([html.Div([html.H3('Titulo de la Barra')],className='sidebar-header'),
              html.Ul([
-                 html.P(['Dummy Heading']),html.Li([html.A(['Home'],href="#homeSubmenu", className="dropdown-toggle"),html.Ul([],className="collapse list-unstyled", id="homeSubmenu")],className='active'),html.Li(['About']),html.Li(['Páginas']),
-                 html.Li(['Portafolio']),html.Li(['Contacto'])
+                 html.P(['Dummy Heading']),
+                 html.Li([html.A(['Home'],href="#homeSubmenu", className="dropdown-toggle"),
+                 html.Ul([html.Li([html.A(['Home1'],href="#")]),html.Li([html.A(['Home2'],href="#")]),html.Li([html.A(['Home3'],href="#")])],className="collapse list-unstyled", id="homeSubmenu")],className='active'),
+                 html.Li([html.A(['Descripción Proyecto'],href='#')]),
+                 html.Li([html.A(['Páginas'],href='#')]),
+                 html.Li([html.A(['Portafolio'],href='#')]),
+                 html.Li([html.A(['Contacto'],href='#')])
              ],className='list-unstyled components'),   
-    
-    ],id='sidebar'),
+        ],id='sidebar'),
     
     html.Div([],id='contenido')
 ],className='wrapper')
